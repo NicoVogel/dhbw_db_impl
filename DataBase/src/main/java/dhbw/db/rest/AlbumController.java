c52855127b0f2ea1deddf4dad098aa4406ecbff4
Nico Vogel <nico-vogel97@outlook.de>
2019-03-27 08:57:39 +0100

    add

---
+++ DataBase/src/main/java/dhbw/db/rest/AlbumController.java
@@ -0,0 +1,10 @@
+package dhbw.db.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/album")
+public class AlbumController {
+
+}
