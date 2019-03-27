76758a4993a188b26661a184d55a68edd991eedb
Nico Vogel <nico-vogel97@outlook.de>
2019-03-27 09:22:01 +0100

    add

---
+++ DataBase/src/main/java/dhbw/db/model/AlbumHasArtist.java
@@ -0,0 +1,14 @@
+package dhbw.db.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AlbumHasArtist {
+
+	private int artistId;
+	private int ablumId;
+}
