7a16be3331ce3f501180d855a2effb70c159243a
Nico Vogel <nico-vogel97@outlook.de>
2019-03-27 09:01:31 +0100

    update

--- DataBase/src/main/java/dhbw/db/DBApplication.java
+++ DataBase/src/main/java/dhbw/db/DBApplication.java
@@ -2,11 +2,19 @@ package dhbw.db;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import dhbw.db.io.DBIO;
 
 @SpringBootApplication
 public class DBApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DBApplication.class, args);
+	}
+	
+	@Bean
+	public DBIO getDBIO() {
+		return new DBIO();
 	}
 }
