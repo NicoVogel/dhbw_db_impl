e869e76822b4586e7bdfd0c0a627c87b7a72049d
Nico Vogel <nico-vogel97@outlook.de>
2019-03-27 09:46:41 +0100

    update

--- DataBase/src/main/java/dhbw/db/rest/DBRestController.java
+++ DataBase/src/main/java/dhbw/db/rest/DBRestController.java
@@ -3,6 +3,7 @@ package dhbw.db.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dhbw.db.io.DBIO;
@@ -12,11 +13,13 @@ import dhbw.db.model.Album;
 @RequestMapping("/")
 public class DBRestController {
 
+	private static final char VALUE_SEPEARATOR = ',';
+	
 	@Autowired
 	private DBIO io;
 
 	@GetMapping("/")
-	public String reload() {
+	public String reload(@RequestParam() String location) {
 		return "";
 	}
 	
