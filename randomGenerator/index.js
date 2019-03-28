af44d3c89b0135a0aad2f3fd44317257452a25fc
Nico Vogel <nico-vogel97@outlook.de>
2019-03-28 15:50:24 +0100

    update

--- randomGenerator/index.js
+++ randomGenerator/index.js
@@ -6,6 +6,46 @@ const amount = 1000000;
 
 let artists = [{ name: "name", year: "year", country: "country" }];
 let albums = [{ name: "name", year: "year", artist: "artist" }];
+let artistNames = [];
+let artistNamesMatches = {};
+
+function str_hash(str) {
+    var hash = 0, i, chr;
+    if (str.length === 0) return hash;
+    for (i = 0; i < str.length; i++) {
+        chr = str.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+function getArtistName() {
+    let name = faker.name.findName()
+    let hash = str_hash(name);
+    let match = artistNames[hash];
+    if (match === undefined) {
+        artistNames[hash] = name;
+        return name;
+    }
+    if (artistNamesMatches[hash] === undefined) {
+        artistNamesMatches[hash] = 1;
+    } else {
+        artistNamesMatches[hash]++;
+    }
+    name += " & ";
+    do {
+        let num = getRandomArbitrary(33, 126);
+        if (num == 44 || num == 92) num++;
+        name += String.fromCharCode(num);
+        hash = str_hash(name);
+        match = artistNames[hash];
+        if (match === undefined) {
+            artistNames[hash] = name;
+            return name;
+        }
+    } while (true);
+}
 
 
 function getRandomArbitrary(min, max) {
@@ -25,9 +65,11 @@ function genArtists() {
 
     let artistDate = getRandomRange(1700, 2019, amount);
     for (let index = 0; index < amount; index++) {
+        let name = getArtistName();
+        let year = artistDate[index];
         artists.push({
-            name: faker.name.findName(),
-            year: artistDate[index],
+            name: name,
+            year: year,
             country: "??"
         });
     }
@@ -40,16 +82,17 @@ function genAlbums() {
     let albumDate = getRandomRange(1700, 2019, amount);
     let albumWordCount = getRandomRange(1, 5, amount);
     for (let index = 0; index < amount; index++) {
+        let name = lorem({
+            count: albumWordCount[index],
+            units: "words"
+        });
+        let year = albumDate[index];
         albums.push({
-            name: lorem({
-                count: albumWordCount[index],
-                units: "words"
-            }),
+            name: name,
             artist: "",
-            year: albumDate[index]
+            year: year
         });
     }
-
     console.log("album base done");
 
     buildReference();
@@ -58,19 +101,19 @@ function genAlbums() {
 function buildReference() {
 
     let artistsPerAlbum = getRandomRange(1, 3, amount);
-    for (let index = 0; index < amount; index++) {
-        const artistsForThisAlbum = artistsPerAlbum[index];
+
+    for (let index = 1; index < amount + 1; index++) {
+        const artistsForThisAlbum = artistsPerAlbum[index - 1];
         const album = albums[index];
 
         let artistIndexes = getRandomRange(1, amount, artistsForThisAlbum);
-
         let albumArtist = [];
 
         for (let index2 = 0; index2 < artistsForThisAlbum; index2++) {
             const artistIndex = artistIndexes[index2];
-            albumArtist.push(artists[artistIndex].name);
+            let artistName = artists[artistIndex].name;
+            albumArtist.push(artistName);
         }
-
         album.artist = albumArtist.join(',');
     }
 
@@ -90,6 +133,19 @@ function writeToFile() {
         await csvAlbum.toDisk('./album.csv');
 
         console.log("done");
+
+        let num = 0;
+        let i = 0;
+        let keys = Object.keys(artistNamesMatches);
+        keys.forEach((element) => {
+            if (artistNamesMatches[element] > num) {
+                num = artistNamesMatches[element];
+                i = element;
+            }
+        });
+        console.log("most used name '" + artistNames[i] + "' with: " + num);
+
+
     })();
 }
 
