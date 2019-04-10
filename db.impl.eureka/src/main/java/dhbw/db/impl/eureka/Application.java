1d586c1961ebd96a32e929e1f4ff12edc7a7ead0
Nico Vogel <nico-vogel97@outlook.de>
2019-04-10 15:43:03 +0200

    finished

--- db.impl.eureka/src/main/java/dhbw/db/impl/eureka/Application.java
+++ db.impl.eureka/src/main/java/dhbw/db/impl/eureka/Application.java
@@ -2,11 +2,14 @@ package dhbw.db.impl.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Application {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "registration-server");
 		SpringApplication.run(Application.class, args);
 	}
 
