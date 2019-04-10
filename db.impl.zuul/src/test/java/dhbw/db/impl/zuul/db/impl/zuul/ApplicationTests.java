352bb53000a37e27155aa549b44a6b54bd429cc7
Nico Vogel <nico-vogel97@outlook.de>
2019-04-10 15:46:44 +0200

    init

---
+++ db.impl.zuul/src/test/java/dhbw/db/impl/zuul/db/impl/zuul/ApplicationTests.java
@@ -0,0 +1,16 @@
+package dhbw.db.impl.zuul.db.impl.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
