716a1b70282a9bd6c953c64cb057d49010e6baef
Nico Vogel <nico-vogel97@outlook.de>
2019-04-12 10:41:10 +0200

    remove authentication

--- db.impl.zuul/src/main/java/dhbw/db/impl/zuul/db/impl/zuul/Application.java
+++ db.impl.zuul/src/main/java/dhbw/db/impl/zuul/db/impl/zuul/Application.java
@@ -4,11 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -18,13 +13,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(user);
-
-	}
-
 }
