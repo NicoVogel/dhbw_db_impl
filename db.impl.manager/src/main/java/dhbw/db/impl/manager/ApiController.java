52000e6117b6ea72f3008e0ea1850703d73295a2
Nico Vogel <nico-vogel97@outlook.de>
2019-04-13 10:14:52 +0200

    test implementation

---
+++ db.impl.manager/src/main/java/dhbw/db/impl/manager/ApiController.java
@@ -0,0 +1,16 @@
+package dhbw.db.impl.manager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ApiController {
+
+	@GetMapping
+	public String getTest() {
+		return "manager works";
+	}
+
+}
