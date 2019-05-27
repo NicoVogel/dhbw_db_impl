458d36a353e7bf75d6d3cfc1de1f89d155d54c09
Nico Vogel <nico-vogel97@outlook.de>
2019-05-27 20:16:11 +0200

    add time logs for queries

--- db.impl.instance/src/main/java/dhbw/db/impl/instance/controller/ApiController.java
+++ db.impl.instance/src/main/java/dhbw/db/impl/instance/controller/ApiController.java
@@ -14,9 +14,11 @@ import dhbw.db.impl.instance.manager.FileManager;
 import dhbw.db.impl.instance.model.Album;
 import dhbw.db.impl.instance.model.Artist;
 import dhbw.db.impl.instance.model.Tupel;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class ApiController {
 
 	@Autowired
@@ -28,34 +30,46 @@ public class ApiController {
 
 	@GetMapping("/q1")
 	public List<Album> getAlbums(@RequestParam int id) {
+		long start = System.currentTimeMillis();
 		Artist artist = artist().read(id);
 		if (artist == null) {
 			throw new DataNotFoundException(
 					String.format("cannot read albums from an artist, because the artist id %d does not exist", id),
 					Artist.class, id);
 		}
-		return artist().getAlbums(artist);
+		List<Album> result = artist().getAlbums(artist);
+		log.info("done with q1, time: {}", System.currentTimeMillis() - start);
+		return result;
 	}
 
 	@GetMapping("/q2")
 	public List<Tupel<Artist, Integer>> getLastReleaseDateOfEachArtist() {
-		return artist().getLastReleaseDateOfEachArtist();
+		long start = System.currentTimeMillis();
+		List<Tupel<Artist, Integer>> result = artist().getLastReleaseDateOfEachArtist();
+		log.info("done with q2, time: {}", System.currentTimeMillis() - start);
+		return result;
 	}
 
 	@GetMapping("/q3")
 	public int getFoundingYear(@RequestParam int id) {
+		long start = System.currentTimeMillis();
 		Artist artist = artist().read(id);
 		if (artist == null) {
 			throw new DataNotFoundException(
 					String.format("cannot read artist founding year, because the artist id %d does not exist", id),
 					Artist.class, id);
 		}
-		return artist.getYear();
+		int year = artist.getYear();
+		log.info("done with q3, time: {}", System.currentTimeMillis() - start);
+		return year;
 	}
 
 	@GetMapping("/q4")
 	public List<Artist> getArtistsWhichHaveNoReleases() {
-		return artist().getArtistsWhichHaveNoReleases();
+		long start = System.currentTimeMillis();
+		List<Artist> result = artist().getArtistsWhichHaveNoReleases();
+		log.info("done with q4, time: {}", System.currentTimeMillis() - start);
+		return result;
 	}
 
 }
