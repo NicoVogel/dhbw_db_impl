2f294d8ac6fb17e2c4967328536526dee252ff38
Nico Vogel <nico-vogel97@outlook.de>
2019-05-27 19:02:00 +0200

    add comment

--- db.impl.instance/src/main/java/dhbw/db/impl/instance/model/Updater.java
+++ db.impl.instance/src/main/java/dhbw/db/impl/instance/model/Updater.java
@@ -2,6 +2,13 @@ package dhbw.db.impl.instance.model;
 
 public interface Updater<T> {
 
+	/**
+	 * update all values with the data from parameter object
+	 * 
+	 * @param object
+	 * @return true if it worked, false it the parameter is null or the id of both
+	 *         do not match
+	 */
 	public boolean update(T object);
 
 }
