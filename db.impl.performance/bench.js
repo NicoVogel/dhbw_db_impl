4612785846521a7f705cb6208a5c30b1256d841c
Nico Vogel <nico-vogel97@outlook.de>
2019-05-29 13:54:32 +0200

    update

--- db.impl.performance/bench.js
+++ db.impl.performance/bench.js
@@ -4,18 +4,18 @@ const flows = [
   {
     main: [{
       get: 'http://5.189.152.84:8080/db/performance/index'
-    }, ]
+    },]
   },
   {
     main: [{
       get: 'http://5.189.152.84:8080/db/performance/'
-    }, ]
+    },]
   }
 ];
 
 const runOptions = {
-  limit: 1000, 
-  iterations: 100
+  limit: 6,
+  iterations: 1000
 };
 
 flows.forEach(flow => {
@@ -33,3 +33,27 @@ flows.forEach(flow => {
       console.log(`\nsum ${stats.main.histogram.sum.toFixed(5)}`);
     });
 })
+
+
+
+
+/*
+5000
+
+URL http://5.189.152.84:8080/db/performance/index
+error count:  0
+min 165.54725
+avg 340.74779
+max 1314.04079
+
+sum 1703738.96501
+
+
+URL http://5.189.152.84:8080/db/performance/
+error count:  0
+min 87.85505
+avg 1430.00560
+max 7625.97066
+
+sum 7150028.01171
+*/
